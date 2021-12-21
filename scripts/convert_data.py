5485caad51a0f26e55aef3c672895385c507d7ab
Mert Çökmez <61047267+coekmez@users.noreply.github.com>
2021-12-21 01:48:02 +0100

    convert_data completed

--- scripts/convert_data.py
+++ scripts/convert_data.py
@@ -1,49 +1,70 @@
 import json, numpy as np
 
 f = open("scripts/non-processed data/rec_7/HandPose_691077791.txt")
-data = json.load(f)
+hand_data = json.load(f)
+f.close()
+
+f = open('scripts/non-processed data/rec_7/activityLabels.txt')
+activity_data = json.load(f)
 f.close()
 
 
-skeletons = data.copy()
+skeletons = hand_data.copy()
 duration = skeletons.pop('recID')
 skeletons.pop('timestamp')
+flag = 1
+#maybe change this as a command line argument?
+setup_id = 1
 
-#remove frames with weird coordinates
-threshold = 100
-cursor = 0
-for id,skeleton in enumerate(skeletons):
-    for temp_cursor in range(duration):
-        if(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['x']) > threshold):
-            continue
-        elif(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['y']) > threshold):
-            continue
-        elif(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['z']) > threshold):
-            continue
-        else:
-            if (temp_cursor > cursor):
-                cursor = temp_cursor
-            break
-
-
-with open('deneme.skeleton', 'w') as f:
+for activity_id,activity in enumerate(activity_data):
+    start_time = activity['Start']
+    stop_time = activity['Stop']
+    if (activity['Label'] == 'bg'):
+        label = 0
+    elif (activity['Label'] == ' bg'):
+        label = 0
+    elif (activity['Label'] == ' grab'):
+        label = 1
     
-    f.write(str(duration - cursor))
-    f.write('\n')
-    for timestep in range(cursor,duration):
-        f.write(str(len(skeletons)))
+    duration = stop_time - start_time
+
+    #remove frames with weird coordinates
+    threshold = 100
+    cursor = start_time
+    for skeleton_id,skeleton in enumerate(skeletons):
+        for temp_cursor in range(start_time,stop_time):
+            if(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['x']) > threshold):
+                continue
+            elif(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['y']) > threshold):
+                continue
+            elif(abs(skeletons[skeleton]['handPose']['Wrist'][temp_cursor]['z']) > threshold):
+                continue
+            else:
+                if (temp_cursor > cursor):
+                    cursor = temp_cursor
+                break
+
+    
+    with open('data/S00{}C001P001R00{}A00{}.skeleton'.format(setup_id,activity_id,label), 'w') as f:
+        f.write(str(duration))
         f.write('\n')
-        for id,skeleton in enumerate(skeletons):
-            if (timestep == cursor):
-                skeletons[skeleton]['handPose'].pop('None')
-            f.write(str(id+1))
+        
+        for timestep in range(cursor,stop_time):
+            f.write(str(len(skeletons)))
             f.write('\n')
-            f.write(str(len(skeletons[skeleton]['handPose'])))
-            f.write('\n')
-            for i, joint in enumerate(skeletons[skeleton]['handPose']):
-                f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['x']))
-                f.write(' ')
-                f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['y']))
-                f.write(' ')
-                f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['z']))
+            
+            for id,skeleton in enumerate(skeletons):
+                if (flag):
+                    skeletons[skeleton]['handPose'].pop('None')
+                    flag = 0
+                f.write(str(id+1))
                 f.write('\n')
+                f.write(str(len(skeletons[skeleton]['handPose'])))
+                f.write('\n')
+                for i, joint in enumerate(skeletons[skeleton]['handPose']):
+                    f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['x']))
+                    f.write(' ')
+                    f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['y']))
+                    f.write(' ')
+                    f.write(str(skeletons[skeleton]['handPose'][joint][timestep]['z']))
+                    f.write('\n')
